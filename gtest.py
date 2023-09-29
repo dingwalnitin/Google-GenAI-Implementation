bb29c88a199c2eb1b9f4b6dba335e58300722706
dingwalnitin <90894005+dingwalnitin@users.noreply.github.com>
2023-09-30 02:11:13 +0530

    Update gtest.py

--- gtest.py
+++ gtest.py
@@ -5,25 +5,13 @@ import os
 
 app = Flask(__name__)
 
-# Replace with your actual API key and model ID
+
 
 apikey = os.environ.get("API_KEY") 
 palm.configure(api_key=apikey)
 model_id = 'models/text-bison-001'
 
-# Function to count unique visitors based on unique IP addresses in the text file
-def count_unique_visitors():
-    unique_ips = set()  # Use a set to store unique IP addresses
-    try:
-        with open('unique_ips.txt', mode='r') as file:
-            for line in file:
-                ip_address = line.strip()
-                unique_ips.add(ip_address)
-        unique_visitors = len(unique_ips)
-    except FileNotFoundError:
-        unique_visitors = 0
 
-    return unique_visitors
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -40,9 +28,9 @@ def generate_text():
         elapsed_time = end_time - start_time
         output = completion.candidates[0]['output']
 
-    unique_visitors = count_unique_visitors()
+    
 
-    return render_template('index.html', output=output, elapsed_time=elapsed_time, unique_visitors=unique_visitors)
+    return render_template('index.html', output=output, elapsed_time=elapsed_time)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
